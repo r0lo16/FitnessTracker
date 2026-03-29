ec80f3cd6ea16c1f233647840fe9e5c91e007cea
Dawid Rolinski <rolkadawid14@gmail.com>
2026-03-29 18:10:05 +0200

    "Complete JPA relations for LAB02"

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -6,10 +6,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
-@Table(name = "statistics")
+@Table(name = "Statistics")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString
@@ -20,21 +19,13 @@ public class Statistics {
     @Nullable
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "user_id", nullable = false, unique = true)
-    private User user;
-
-    @Column(name = "total_trainings", nullable = false)
     private int totalTrainings;
 
-    @Column(name = "total_distance", nullable = false)
     private double totalDistance;
 
-    @Column(name = "total_calories_burned", nullable = false)
     private int totalCaloriesBurned;
 
-    public Statistics(User user, int totalTrainings, double totalDistance, int totalCaloriesBurned) {
-        this.user = user;
+    public Statistics(int totalTrainings, double totalDistance, int totalCaloriesBurned) {
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
